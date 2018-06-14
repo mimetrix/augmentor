585cd666633728e95058742eca4b735b85c7e87a
David <drusso12234@gmail.com>
2018-06-14 11:28:19 -0700

    Added better error handling and fixed a bug

--- KafkaAlter.py
+++ KafkaAlter.py
@@ -1,5 +1,6 @@
 from pykafka import KafkaClient
 from pykafka.common import OffsetType
+from pykafka.exceptions import NoBrokersAvailableError, UnknownTopicOrPartition
 import json
 import threading
 import logging
@@ -8,19 +9,25 @@ import sys
 import GeoIP
 PYHTONHASHSEED = 0
 
-if sys.argv[1] is "h" or "-h":
+if sys.argv[1] is "h":
     print "arg1=kafka client\narg2=topic to consume from\narg3=topic to produce to"
     raise SystemExit
-
-geo = GeoIP.open("GeoLiteCity.dat", GeoIP.GEOIP_STANDARD)
-client = KafkaClient(hosts= sys.argv[1])
-topic = client.topics[sys.argv[2]] #Topic to consume from
-nTopic = client.topics[sys.argv[3]] #Topic to produce to
-consumer = topic.get_simple_consumer(
-    consumer_group = sys.argv[2],
-    auto_offset_reset = OffsetType.LATEST,
-    reset_offset_on_start = True)
-producer = nTopic.get_sync_producer()
+try:
+    geo = GeoIP.open("GeoLiteCity.dat", GeoIP.GEOIP_STANDARD)
+    client = KafkaClient(hosts= sys.argv[1])
+    topic = client.topics[sys.argv[2]] #Topic to consume from
+    nTopic = client.topics[sys.argv[3]] #Topic to produce to
+    consumer = topic.get_simple_consumer(
+        consumer_group = sys.argv[2],
+        auto_offset_reset = OffsetType.LATEST,
+        reset_offset_on_start = True)
+    producer = nTopic.get_sync_producer()
+except NoBrokersAvailableError:
+    print "Please enter a valid Kafka Broker"
+    raise SystemExit
+except UnknownTopicOrPartition:
+    print "Please enter a valid topic"
+    raise SystemExit
 
 try:
     for message in consumer:
