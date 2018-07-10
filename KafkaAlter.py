c682bcba85b64f9a8a76956d1c6d737fbe8917da
David <drusso12234@gmail.com>
2018-07-10 07:00:57 -0700

    added dstIP support

--- KafkaAlter.py
+++ KafkaAlter.py
@@ -37,9 +37,39 @@ try:
             value = nDict.values()
             ipDict = nDict["ipv4"] #Loads the keys inside ipv4 into a new dict
             geoL = geo.record_by_addr(ipDict["srcAddr"]) #Get Lat/Long from ip
+            geoLd = geo.record_by_addr(ipDict["dstAddr"])
+            sDict = {}
+            dDict = {}
             if geoL is not None:
-                nDict['Latitude'] = geoL['latitude']
-                nDict['Longitude'] = geoL['longitude']
+                sDict['Latitude'] = geoL['latitude']
+                sDict['Longitude'] = geoL['longitude']
+                sDict['City'] = geoL['city']
+                sDict['RegionName'] = geoL['region_name']
+                sDict['Region'] = geoL['region']
+                sDict['areaCode'] = geoL['area_code']
+                sDict['TimeZone'] = geoL['time_zone']
+                sDict['MetroCode'] = geoL['metro_code']
+                sDict['CountryCode3'] = geoL['country_code3']
+                sDict['PostalCode'] = geoL['postal_code']
+                sDict['dmaCode'] = geoL['dma_code']
+                sDict['CountryCode'] = geoL['country_code']
+                sDict['CountryName'] = geoL['country_name']
+                nDict['src'] = sDict
+            if geoLd is not None:
+                dDict['Latitude'] = geoLd['latitude']
+                dDict['Longitude'] = geoLd['longitude']
+                dDict['City'] = geoLd['city']
+                dDict['RegionName'] = geoLd['region_name']
+                dDict['Region'] = geoLd['region']
+                dDict['areaCode'] = geoLd['area_code']
+                dDict['TimeZone'] = geoLd['time_zone']
+                dDict['MetroCode'] = geoLd['metro_code']
+                dDict['CountryCode3'] = geoLd['country_code3']
+                dDict['PostalCode'] = geoLd['postal_code']
+                dDict['dmaCode'] = geoLd['dma_code']
+                dDict['CountryCode'] = geoLd['country_code']
+                dDict['CountryName'] = geoLd['country_name']
+                nDict['dst'] = dDict
             js = json.dumps(nDict)
             producer.produce(js)
         except ValueError:
